a3459ee89cf7cac432524a40ef40b2d08dc311c2
jakeschievink <jakeschievink@gmail.com>
2013-10-20 15:53:59 -0700

    Commited recent changes

--- MotionSensingArduino/MotionSensingArduino.ino
+++ MotionSensingArduino/MotionSensingArduino.ino
@@ -1,11 +1,12 @@
 void setup(){
   Serial.begin(9600);
+  delay(1000);
 }
 void loop(){
   int motionval = digitalRead(13);
-  Serial.println(motionval);
+  Serial.println(motionval, DEC);
   if(motionval == 1){
-    delay(1000);
+    delay(600000);
   }else{
     delay(100);
   }
