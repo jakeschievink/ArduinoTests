61f62553b3b07315118c723c7f28b541a7c0c211
jakeschievink <jakeschievink@gmail.com>
2013-11-24 23:28:39 -0500

    Added usable, min max code

--- AmbianceInterPerater/AmbianceInterPerater.ino
+++ AmbianceInterPerater/AmbianceInterPerater.ino
@@ -1,18 +1,18 @@
 #include <Thermistor.h>
 #define DEBUG
-#define READINGINTERVAL 10
-#define NUMBEROFREADINGS 20
+#define READINGINTERVAL 1000
+#define NUMBEROFREADINGS 10
 //pinSetups
 const int ledBluePin = 10;
-const int ledYellowPin = 11;
+const int ledGreenPin = 11;
 const int ledRedPin = 9;
 const int ledWhitePin = 6;
-const int sensorPin  = A1;
+const int photoPin  = A1;
 const int micPin  = A4;
 
-long time,readingtime;
+long time,readingTime, longReadingTime;
 
-typedef struct Readvals{
+struct readVals{
         int reading;
         int readings[NUMBEROFREADINGS];
         int readingPos;
@@ -21,32 +21,47 @@ typedef struct Readvals{
         int max;
         int min;
 };
-struct Readvals mic;
-struct Readvals photo;
-struct Readvals therm;
+const struct readVals DEFAULTVALS={0,125,0,0,0,0,1000};
+struct readVals mic = DEFAULTVALS;
+struct readVals photo = DEFAULTVALS;
+struct readVals therm;
+
 Thermistor temp(5);
 void setup(){
         pinMode(ledBluePin, OUTPUT);
-        pinMode(ledYellowPin, OUTPUT);
+        pinMode(ledGreenPin, OUTPUT);
         pinMode(ledRedPin, OUTPUT);
+        pinMode(ledWhitePin, OUTPUT);
         Serial.begin(9600);
-        readingtime = 1000;
+        readingTime = 1000;
 }
 void loop(){
         time = millis();
          
-        if(time > readingtime){
+        if(time > readingTime){
                 takeReadings();
-                readingtime = time+READINGINTERVAL;
+                readingTime = time+READINGINTERVAL;
                 #if defined DEBUG
-                        Serial.print("photo: ");
-                        Serial.println(photo.reading);
+                        Serial.print("photodisplayed: ");
+                        Serial.println(photo.displayed);
+                        Serial.print("photovalue: ");
+                        Serial.println(photo.value);
+                        Serial.print("photomin: ");
+                        Serial.println(photo.min);
+                        Serial.print("photomax: ");
+                        Serial.println(photo.max);
+                        Serial.print("photoavg: ");
+                        Serial.println(average(photo.readings));
+
+
                         Serial.print("mic: ");
                         Serial.println(mic.reading);
                         Serial.print("Temp:");
                         Serial.println(therm.reading);
                 #endif
         }
+        if(time > longReadingTime){
+       }
         
         
         
@@ -58,15 +73,39 @@ void loop(){
 
         delay(90);
 }
+void writetoLeds(){
+        analogWrite(ledWhitePin, photo.displayed);
+        analogWrite(ledBluePin,255-photo.displayed);
+        analogWrite(ledGreenPin, mic.displayed);
+        analogWrite(ledRedPin, therm.value);
+}
+void takeMinMax(struct readVals *comp){
+        if((*comp).min == 0){
+                (*comp).min = 1000;
+        }
 
+        if(average((*comp).readings) < (*comp).min){
+                (*comp).min = average((*comp).readings);
+                
+        }else if(average((*comp).readings) > (*comp).max){
+                (*comp).max = average((*comp).readings);
+        }
+
+}
+void takeReading(struct readVals *comp, int pin){
+        (*comp).reading = analogRead(pin);
+        (*comp).readingPos = insert((*comp).readings, (*comp).reading, (*comp).readingPos);
+        takeMinMax(&*comp);
+        (*comp).value = map(average((*comp).readings), (*comp).min, (*comp).max, 0, 255);
+}
 void takeReadings(){
-                photo.reading = analogRead(sensorPin);
-                photo.readingPos = insert(photo.readings, photo.reading, photo.readingPos);
-                photo.value = map(average(photo.readings),0,1024, 0, 255);
+
+               takeReading(&photo, photoPin); 
 
                 mic.reading = abs(analogRead(micPin)-840);
                 mic.readingPos = insert(mic.readings, mic.reading, mic.readingPos);
                 mic.value = average(mic.readings);
+                takeMinMax(&mic);
                 //mic.value = map(average(mic.readings),50,255,0,255);
 
                 therm.reading = int(temp.getTemp());
@@ -76,17 +115,12 @@ void updateDisplayedValues(){
         photo.displayed = photo.displayed*.9+.1*photo.value;
         mic.displayed = mic.displayed*.9+.1*mic.value;
 }
-void writetoLeds(){
-        analogWrite(ledWhitePin, photo.displayed);
-        analogWrite(ledBluePin,255-photo.displayed);
-        analogWrite(ledYellowPin, mic.displayed);
-        analogWrite(ledRedPin, therm.value);
-}
+
 int insert(int arr[], int item, int pos){
         if(pos == 11){
                 return 0;
         }else{
-                arr[pos]=abs(item);
+                arr[pos]=item;
                 pos++;
                 return pos;
         }
