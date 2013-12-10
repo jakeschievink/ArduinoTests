8a53636b4e8d6cf6312bcfcc20bf6f84d5c58fd8
jakeschievik <jakeschievik@gmail.com>
2013-12-10 13:46:44 -0500

    commits

--- AmbianceInterPerater/AmbianceInterPerater.ino
+++ AmbianceInterPerater/AmbianceInterPerater.ino
@@ -1,15 +1,14 @@
 #include <Thermistor.h>
 #define DEBUG
-#define READINGINTERVAL 1000
-#define NUMBEROFREADINGS 10
+#define READINGINTERVAL 100
+#define NUMBEROFREADINGS 20
 //pinSetups
-const int ledBluePin = 10;
-const int ledGreenPin = 11;
-const int ledRedPin = 9;
-const int ledWhitePin = 6;
-const int photoPin  = A1;
-const int micPin  = A4;
-
+const int ledBluePin = 9;
+const int ledGreenPin = 10;
+const int ledRedPin = 11;
+const int photoPin  = A0;
+const int micPin  = A2;
+const int motionPin  = 2;
 long time,readingTime, longReadingTime;
 
 struct readVals{
@@ -21,23 +20,24 @@ struct readVals{
         int max;
         int min;
 };
-const struct readVals DEFAULTVALS={0,125,0,0,0,0,1000};
+const int defaultreadings[NUMBEROFREADINGS] = { 125 };
+const struct readVals DEFAULTVALS={0,*defaultreadings,0,0,0,0,1000};
 struct readVals mic = DEFAULTVALS;
 struct readVals photo = DEFAULTVALS;
-struct readVals therm;
+//struct readVals therm;
+struct readVals motion;
 
-Thermistor temp(5);
+//Thermistor temp(1);
 void setup(){
         pinMode(ledBluePin, OUTPUT);
         pinMode(ledGreenPin, OUTPUT);
         pinMode(ledRedPin, OUTPUT);
-        pinMode(ledWhitePin, OUTPUT);
         Serial.begin(9600);
         readingTime = 1000;
 }
 void loop(){
         time = millis();
-         
+ //Takes Sensor readings at a specified interval        
         if(time > readingTime){
                 takeReadings();
                 readingTime = time+READINGINTERVAL;
@@ -51,20 +51,20 @@ void loop(){
                         Serial.print("photomax: ");
                         Serial.println(photo.max);
                         Serial.print("photoavg: ");
-                        Serial.println(average(photo.readings));
+                        Serial.println(*photo.readings);
 
 
-                        Serial.print("mic: ");
-                        Serial.println(mic.reading);
-                        Serial.print("Temp:");
-                        Serial.println(therm.reading);
+                        Serial.print("mic displayed: ");
+                        Serial.println(mic.displayed);
+
+                        Serial.print("Motion:");
+                        for(int i = 0; i < NUMBEROFREADINGS; i++){
+                                Serial.print(motion.readings[i]);
+                        }
+                        Serial.println(motion.value);
                 #endif
         }
-        if(time > longReadingTime){
-       }
-        
-        
-        
+       //updated the values displayed to smooth the color transitions 
         updateDisplayedValues();
         //writing to Leds
         writetoLeds();
@@ -74,10 +74,9 @@ void loop(){
         delay(90);
 }
 void writetoLeds(){
-        analogWrite(ledWhitePin, photo.displayed);
         analogWrite(ledBluePin,255-photo.displayed);
         analogWrite(ledGreenPin, mic.displayed);
-        analogWrite(ledRedPin, therm.value);
+        analogWrite(ledRedPin, motion.displayed*.7);
 }
 void takeMinMax(struct readVals *comp){
         if((*comp).min == 0){
@@ -85,10 +84,10 @@ void takeMinMax(struct readVals *comp){
         }
 
         if(average((*comp).readings) < (*comp).min){
-                (*comp).min = average((*comp).readings);
+                (*comp).min = abs(average((*comp).readings));
                 
         }else if(average((*comp).readings) > (*comp).max){
-                (*comp).max = average((*comp).readings);
+                (*comp).max = abs(average((*comp).readings));
         }
 
 }
@@ -104,18 +103,31 @@ void takeReadings(){
 
                 mic.reading = abs(analogRead(micPin)-840);
                 mic.readingPos = insert(mic.readings, mic.reading, mic.readingPos);
-                mic.value = average(mic.readings);
                 takeMinMax(&mic);
-                //mic.value = map(average(mic.readings),50,255,0,255);
+                mic.value = map(average(mic.readings), mic.min, mic.max, 0, 255);
+                //mic.value = map(average(mic.readings),50,255,0,255);*/
+                motionReading();
+                takeMinMax(&motion);
+                motion.value = map(average(motion.readings), motion.min, motion.max, 0, 255);
 
-                therm.reading = int(temp.getTemp());
-                therm.value = map(therm.reading, 155, 200, 0, 255);
+                //therm.reading = int(temp.getTemp());
+                //therm.value = map(therm.reading, 155, 200, 0, 255);
+}
+void motionReading(){
+        if(digitalRead(motionPin) == 1){
+                motion.reading = 1024;
+        }else{
+                motion.reading = 0;
+        }
+        motion.readingPos = insert(motion.readings, motion.reading, motion.readingPos);
 }
 void updateDisplayedValues(){
+        //Smpooths the displayed values
         photo.displayed = photo.displayed*.9+.1*photo.value;
         mic.displayed = mic.displayed*.9+.1*mic.value;
+        motion.displayed = motion.displayed*.9+.1*motion.value;
 }
-
+//Adds item to readings array
 int insert(int arr[], int item, int pos){
         if(pos == 11){
                 return 0;
@@ -125,6 +137,7 @@ int insert(int arr[], int item, int pos){
                 return pos;
         }
 }
+//returns average of an array
 int average(int arr[]){
         int total = 0;
         for(int i = 0; i < NUMBEROFREADINGS; i++) {
